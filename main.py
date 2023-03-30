fb8481cbe30c9d23a352c0d5818468ebb7cc4810
Not Mark <92692104+PocketEngineer1@users.noreply.github.com>
2023-03-30 17:28:18 -0400

    Function

--- main.py
+++ main.py
@@ -1,49 +1,55 @@
 import shutil, time, zipfile, os, datetime
 
-vault = {
-  'vaults': [
-    {
-      'name': 'TempVault',
-      'root': './TempVault',
-      'main_io': 'Main IO',
-      'working_dir': 'Working',
-      'backup_dir': 'Backups',
-      'snapshot_dir': 'Snapshots',
-      'restore_point_dir': 'Restore Points'
-    }
-  ],
-  'current': 0
-}
-
-# Source and destination directories
-src_dir = vault['vaults'][vault['current']]['root']+'/'+vault['vaults'][vault['current']]['main_io']
-dst_dir = vault['vaults'][vault['current']]['root']+'/'+vault['vaults'][vault['current']]['working_dir']+'/Files'
-
 # Interval in seconds between each copy
 # interval = 60 * 60  # 1 hour
 interval = 10
 
-while True:
-  now = datetime.datetime.now()
-  try:
-    # Copy all files and directories from source to destination
-    shutil.rmtree(dst_dir)
-    shutil.copytree(src_dir, dst_dir)
-    print(f'Copied contents of {src_dir} to {dst_dir}')
-    with zipfile.ZipFile(vault['vaults'][vault['current']]['root']+'/'+vault['vaults'][vault['current']]['working_dir']+'/temp.backup', 'w') as f:
-      for root, dirs, files in os.walk(dst_dir):
-        for dir in dirs:
-          f.write(os.path.join(root, dir), os.path.join(root, dir).split(vault['vaults'][vault['current']]['root']+'/'+vault['vaults'][vault['current']]['working_dir']+'/Files')[1])
-        for file in files:
-          f.write(os.path.join(root, file), os.path.join(root, file).split(vault['vaults'][vault['current']]['root']+'/'+vault['vaults'][vault['current']]['working_dir']+'/Files')[1])
-        f.close()
-    
-    shutil.copy(vault['vaults'][vault['current']]['root']+'/'+vault['vaults'][vault['current']]['working_dir']+'/temp.backup', vault['vaults'][vault['current']]['root']+'/'+vault['vaults'][vault['current']]['backup_dir']+now.strftime("/%Y\%m\%d %H:%M:%S.backup"))
-    os.remove(vault['vaults'][vault['current']]['root']+'/'+vault['vaults'][vault['current']]['working_dir']+'/temp.backup')
+class vault:
+  data = {
+    'vaults': [
+      {
+        'name': 'TempVault',
+        'root': './TempVault',
+        'main_io': 'Main IO',
+        'working_dir': 'Working',
+        'backup_dir': 'Backups',
+        'snapshot_dir': 'Snapshots',
+        'restore_point_dir': 'Restore Points'
+      }
+    ],
+    'active_vaults': [0]
+  }
 
-    print('Created backup of the current vault')
-  except Exception as e:
-    print(f'Error while copying: {e}')
+  def backup(vault_number: int):
+    # Source and destination directories
+    src_dir = vault.data['vaults'][vault_number]['root']+'/'+vault.data['vaults'][vault_number]['main_io']
+    dst_dir = vault.data['vaults'][vault_number]['root']+'/'+vault.data['vaults'][vault_number]['working_dir']+'/Files'
+
+    now = datetime.datetime.now()
+    try:
+      # Copy all files and directories from source to destination
+      shutil.rmtree(dst_dir)
+      shutil.copytree(src_dir, dst_dir)
+      print(f'Copied contents of {src_dir} to {dst_dir}')
+      with zipfile.ZipFile(vault.data['vaults'][vault_number]['root']+'/'+vault.data['vaults'][vault_number]['working_dir']+'/temp.backup', 'w') as f:
+        for root, dirs, files in os.walk(dst_dir):
+          for dir in dirs:
+            f.write(os.path.join(root, dir), os.path.join(root, dir).split(vault.data['vaults'][vault_number]['root']+'/'+vault.data['vaults'][vault_number]['working_dir']+'/Files')[1])
+          for file in files:
+            f.write(os.path.join(root, file), os.path.join(root, file).split(vault.data['vaults'][vault_number]['root']+'/'+vault.data['vaults'][vault_number]['working_dir']+'/Files')[1])
+          f.close()
+      
+      shutil.copy(vault.data['vaults'][vault_number]['root']+'/'+vault.data['vaults'][vault_number]['working_dir']+'/temp.backup', vault.data['vaults'][vault_number]['root']+'/'+vault.data['vaults'][vault_number]['backup_dir']+now.strftime("/%Y %m %d %H %M %S.backup"))
+      os.remove(vault.data['vaults'][vault_number]['root']+'/'+vault.data['vaults'][vault_number]['working_dir']+'/temp.backup')
+      temp = vault.data['vaults'][vault_number]['name']
+      print(f'Created backup of the files in the vault \'{temp}\'')
+      del temp
+    except Exception as e:
+      print(f'Error while copying: {e}')
+
+while True:
+  for i in vault.data['active_vaults']:
+    vault.backup(i)
 
   # Wait for the interval before copying again
   time.sleep(interval)
